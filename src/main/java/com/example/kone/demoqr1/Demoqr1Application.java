569930c9de4f1e399e0477bce066cbc7bc708a56
vinnycrm <vinnycrm@gmail.com>
2023-01-24 21:16:11 +0530

    first commit

---
+++ src/main/java/com/example/kone/demoqr1/Demoqr1Application.java
@@ -0,0 +1,13 @@
+package com.example.kone.demoqr1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoqr1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoqr1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kone/demoqr1/Demoqr1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kone.demoqr1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoqr1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
